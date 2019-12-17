008cc2054a137e207906d7a954d599430799f5a1
shane timmerman <shanetimmerman@gmail.com>
2019-12-17 16:31:07 -0500

    Floated image left

--- src/resume/jobs.js
+++ src/resume/jobs.js
@@ -1,6 +1,6 @@
 import jobData from './data/jobs.json'
 import React from 'react'
-import { Header, Item, List, Segment } from 'semantic-ui-react'
+import { Header, Item, List, Segment, Grid } from 'semantic-ui-react'
 
 let assets = require.context('./assets', true)
 
@@ -14,38 +14,38 @@ function Jobs() {
 
 }
 
-// TODO: CHANGE TO ITEMS
 function Job(props) {
   let { data } = props
   return <Segment raised>
     <Item>
-      <Item.Image src={assets(data.image)} size='medium' circular bordered />
-      <Item.Content>
-        <Item.Header as='h2'>{data.company}</Item.Header>
+      <Segment basic clearing>
+        <Item.Image src={assets(data.image)} floated='left' size='small' circular bordered />
+        <Item.Header as='h1'>{data.company}</Item.Header>
         <Item.Meta as='h3'>{data.location}</Item.Meta>
-        <Item.Content>
-          {data.roles.map(position => <Position key={position.title} data={position} />)}
-        </Item.Content>
-      </Item.Content>
+      </Segment>
+      <Item.Group divided>
+        {data.roles.map(position => <Position key={position.title} data={position} className='resumeJobsPosition'/>)}
+      </Item.Group>
     </Item>
   </Segment>
 }
 
 function Position(props) {
   let { data } = props
-  return <div>
-    <Header as='h4'>{data.title}</Header>
-    <Header as='h5'>{data.dates}</Header>
-    <List bulleted>
-      {data.bullets.map(bullet => <Bullet key={bullet} data={bullet}/>)}
-    </List>
-  </div>
-}
-
-function Bullet(props) {
-  let { data } = props
   return (
-    <List.Item>{data}</List.Item>
+    <Item>
+      <Grid columns={2} stackable>
+        <Grid.Column width={6}>
+          <Item.Header as='h4'>{data.title}</Item.Header>
+          <Item.Header as='h5'>{data.dates}</Item.Header>
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <List bulleted>
+            {data.bullets.map(bullet => <List.Item>{bullet}</List.Item>)}
+          </List>
+        </Grid.Column>    
+      </Grid>
+    </Item>
   )
 }
 
