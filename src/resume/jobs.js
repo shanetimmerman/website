96b1a97aeefe824ddf68335fa24d142efa368307
shane timmerman <shanetimmerman@gmail.com>
2019-12-17 20:20:46 -0500

    Formatting

--- src/resume/jobs.js
+++ src/resume/jobs.js
@@ -5,29 +5,32 @@ import { Header, Item, List, Segment, Grid } from 'semantic-ui-react'
 let assets = require.context('./assets', true)
 
 function Jobs() {
-  return <div>
-    <Header textAlign='center' as='h2'>Work Experience</Header>
-    <Item.Group>
-      {jobData.map(job => <Job key={job.company} data={job} />)}
-    </Item.Group>
-  </div>
-
+  return (
+    <div>
+      <Header textAlign='center' as='h2'>Work Experience</Header>
+      <Item.Group>
+        {jobData.map(job => <Job key={job.company} data={job} />)}
+      </Item.Group>
+    </div>
+  )
 }
 
 function Job(props) {
   let { data } = props
-  return <Segment raised>
-    <Item>
-      <Segment basic clearing>
-        <Item.Image src={assets(data.image)} floated='left' size='small' circular bordered />
-        <Item.Header as='h1'>{data.company}</Item.Header>
-        <Item.Meta as='h3'>{data.location}</Item.Meta>
-      </Segment>
-      <Item.Group divided>
-        {data.roles.map(position => <Position key={position.title} data={position} className='resumeJobsPosition'/>)}
-      </Item.Group>
-    </Item>
-  </Segment>
+  return (
+    <Segment raised>
+      <Item>
+        <Segment basic clearing>
+          <Item.Image src={assets(data.image)} floated='left' size='small' circular bordered />
+          <Item.Header as='h1'>{data.company}</Item.Header>
+          <Item.Meta as='h3'>{data.location}</Item.Meta>
+        </Segment>
+        <Item.Group divided>
+          {data.roles.map(position => <Position key={position.title} data={position} className='resumeJobsPosition'/>)}
+        </Item.Group>
+      </Item>
+    </Segment>
+  )
 }
 
 function Position(props) {
