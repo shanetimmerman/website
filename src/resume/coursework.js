efcdc93b2f3e53c0013b6436fb308038f002d587
shane timmerman <shanetimmerman@gmail.com>
2019-12-17 20:24:25 -0500

    Removed unnecessary tags:

--- src/resume/coursework.js
+++ src/resume/coursework.js
@@ -4,19 +4,15 @@ import { Header, Segment} from 'semantic-ui-react'
 
 function CourseWork() {
   return (
-  <div>
-    <Header textAlign='center' as='h2'>Coursework</Header>
-    <Segment raised>
-      <Header as='h3'>Computer Science Courses</Header>
-      <div>
+    <div>
+      <Header textAlign='center' as='h2'>Coursework</Header>
+      <Segment raised>
+        <Header as='h3'>Computer Science Courses</Header>
         {courseData.relevant.map(course => <p key={course}>{course}</p>)}
-      </div>
-      <Header as='h3'>Additional Courses</Header>
-      <div>
+        <Header as='h3'>Additional Courses</Header>
         {courseData.additional.map(course => <p key={course}>{course}</p>)}
-      </div>
-      </Segment>
-  </div>
+        </Segment>
+    </div>
   )
 }
 
