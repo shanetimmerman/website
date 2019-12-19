ee71b27ea729510e4c86956033c6e136b1871755
shane timmerman <shanetimmerman@gmail.com>
2019-12-19 00:34:11 -0500

    Flushed out project descriptions

--- src/home/index.js
+++ src/home/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
-import { Container, Header, Grid, Image, List, Segment, Icon } from 'semantic-ui-react';
+import { Container, Header, Grid, Image, Segment, Icon } from 'semantic-ui-react';
 
 import profilePhoto from './assets/profile_photo.jpeg'
 
@@ -12,6 +11,9 @@ function Home() {
         <Grid columns={2} relaxed>
             <Grid.Column>
               <Image src={profilePhoto} />
+              <p>This is me ^</p>
+            </Grid.Column>
+            <Grid.Column>
               <p>
                 I'm a tinkerer interested in exploring how software can enable human
                 activities!
@@ -28,41 +30,7 @@ function Home() {
                   <Icon name='github square' />
                   shanetimmerman
                 </Header.Content>
-                <Header.Content href='https://github.com/shanetimmerman' target='_blank' rel='noopener noreferrer' as='a'>
-                  <Icon name='github square' />
-                  shanetimmerman
-                </Header.Content>
               </Header>
-            </Grid.Column>
-            <Grid.Column>
-              <p>
-                Who is Shane? Why did he make this website?
-                Great questions.
-              </p>
-              <p>
-                Wanna see my qualifications? Weird but ok. Check it out 
-                <Link to="/resume">here</Link>
-              </p>
-              <p>
-                You mahy be asking yourself: can this kid even code? Well:
-                <List ordered>
-                  <List.Item>
-                    I have a website so I at least can do some basic HTML.
-                  </List.Item>
-                  <List.Item>
-                    This website is in React because I actually don't know HTML so 
-                    this is easier.
-                  </List.Item>
-                  <List.Item>
-                    Yes! Don't believe me? We trust is important, but you can also 
-                    check out some things I've done 
-                    <Link to="/projects">here</Link>
-                  </List.Item>
-                </List> 
-              </p>
-              <p>
-                I also take pictures! If you wanna check them out, you can find them <Link to="/pics">here</Link>
-              </p>
             </Grid.Column>
           </Grid>
       </Segment>
--- src/projects/projects.js
+++ src/projects/projects.js
@@ -6,56 +6,56 @@ import TrafficForcasting from './traffic_forcasting'
 
 export default [
   {
-    title: "Netflix Recommendation through Apriori Frequency Itemset Mining",
-    git: "https://github.com/shanetimmerman/apriori-frequent-itemset-mining",
-    dates: "2019",
-    summary: "MapReduce program to do Frequent Itemset Mining on the 2009 Netflix prize data, using association rules as a proxy for movie recommendation, much like Youtube did in its early days.",
-    image: "./netflix_icon.jpeg",
+    title: 'Netflix Recommendation through Apriori Frequency Itemset Mining',
+    git: 'https://github.com/shanetimmerman/apriori-frequent-itemset-mining',
+    dates: '2019',
+    summary: 'MapReduce program to do Frequent Itemset Mining on the 2009 Netflix prize data, using association rules as a proxy for movie recommendation, much like Youtube did in its early days. The algorithm uses an apriori based approach, translated into iterative MapReduce jobs written in Java run in AWS EMR.',
+    image: './netflix_icon.jpeg',
     more: <FrequencyItemset />,
   },
   {
-    title: "Studor",
-    git: "https://github.com/shanetimmerman/studor",
-    dates: "2018",
-    summary: "Pheonix and React Web application for tutoring services. Allows tutots to post and hold tutoring sessions with a shared white board, chat, and video call, and students to register and pay for tutoring sessions.",
-    image: "./stutor_icon.jpg",
+    title: 'Studor',
+    git: 'https://github.com/shanetimmerman/studor',
+    dates: '2018',
+    summary: 'Pheonix and React Web application for tutoring services. Allows tutots to post and hold tutoring sessions with a shared white board, chat, and video call, and students to register and pay for tutoring sessions. The applicaiton also supports paid transactions through the PayPal API. The application was written in Elixir though Phoenix and JS through React, using Redux for consolodated state, and PeerJS to handle messaging, Formik to streamline form development, and PostgreSQL for data storage.',
+    image: './stutor_icon.jpg',
     more: <Stutor />
   },
   {
-    title: "Weather Driven Traffic Forcasting",
-    git: "https://github.com/jennder/DCRNN_PyTorch/tree/weather_model",
-    dates: "2019",
-    summary: "Final project for introduction to computer science research. Aimed to extend the Diffuse Concurrent Neural Network from (Li et al. 2017) to include weather prediciton data to challange the prior that traffic patterns are a closed system.<br/>Project incomplete within the allotted course time.",
-    image: "./dcrnn_icon.png",
+    title: 'Weather Driven Traffic Forcasting',
+    git: 'https://github.com/jennder/DCRNN_PyTorch/tree/weather_model',
+    dates: '2019',
+    summary: 'Final project for introduction to computer science research. Aimed to extend the Diffuse Concurrent Neural Network from (Li et al. 2017) to include weather prediciton data to challange the prior that traffic patterns are a closed system.<br/>Project incomplete within the allotted course time. The model was written primarily by liyaguang, with slight modifications to enable weather data ingestion, and was composed in PyTorch.',
+    image: './dcrnn_icon.png',
     abandoned: true,
     more: <TrafficForcasting />
   },
   {
-    title: "Santorini",
-    git: "https://github.com/jennder/DCRNN_PyTorch/tree/weather_model",
-    date: "2018",
-    summary: "An implementation of the 2004 Kickstarter game Santorini. Players take turns building a town by placing pieces. Once a player moves to the thrid level of a town, that player wins. The game supports player vs. player, player vs. AI, and AI vs. AI, where players can propose TCP connected AIs to match against one another.",
-    image: "./santorini_box_art.png",
+    title: 'Santorini',
+    git: 'https://github.com/jennder/DCRNN_PyTorch/tree/weather_model',
+    date: '2018',
+    summary: 'An implementation of the 2004 Kickstarter game Santorini. Players take turns building a town by placing pieces. Once a player moves to the thrid level of a town, that player wins. The game supports player vs. player, player vs. AI, and AI vs. AI, where players can propose TCP connected AIs to match against one another. The application is written entirely in Python3.',
+    image: './santorini_box_art.png',
   },
   {
-    title: "Swim Web App",
-    git: "https://github.com/shanetimmerman/swim_web_app",
-    dates: "2019",
-    summary: "Web application for managing a swim league developed as a final project for CS 3200 Database Design. The project uses MySQL as its DBSM, and has a SpringBoot backend with a React and Bootstrap client.",
-    image: "./my_sql_icon.jpg"
+    title: 'Swim Web App',
+    git: 'https://github.com/shanetimmerman/swim_web_app',
+    dates: '2019',
+    summary: 'Web application for managing a swim league developed as a final project for CS 3200 Database Design. It enables control over teams, coaches, swim leagues, individual swimmers, meets, races, and records. While the UI is simplistic, the exposed API provides a system for mass data ingrestion to help facilitate league management. The project uses MySQL as its DBSM, and has a SpringBoot backend with a React and Bootstrap client.',
+    image: './my_sql_icon.jpg'
   },
   {
-    title: "Task Tracker",
-    git: "https://github.com/shanetimmerman/task_tracker_single_page_app",
-    dates: "2018",
-    summary: "Web app allowing users to manage tasks for themselves and their subordinates. The application is written in Elixir and React, using the Phoenix framework and Redux.",
-    image: "./phoenix_icon.png",
+    title: 'Task Tracker',
+    git: 'https://github.com/shanetimmerman/task_tracker_single_page_app',
+    dates: '2018',
+    summary: 'A minimalistic task tracker web application. Users can easily view and manage their unfinished tasks, create new ones for themselves or teammates, and edit timeslines and associated tasks. The application is written in Elixir and React, using the Phoenix framework and Redux, and a Boostrap UI, and uses PostreSQL to manage the data.',
+    image: './phoenix_icon.png',
   },
   {
     title: 'Spotify Desktop Genius',
     git: 'https://github.com/shanetimmerman/HBP2018',
     dates: '2018',
-    summary: 'Kivy desktop application emulating Spotify and Genius\' “Behind the Lyrics” mobile feature. Users can control and listen to Spotify music through the Spotify API while viewing synced lyrical and song information scraped from the web',
+    summary: 'Kivy desktop application emulating Spotify and Genius\' “Behind the Lyrics” mobile feature. I personally mainly use Spotify\'s desktop application when I am working, but I like being able to see the lyrics and narrative provied by Genius for that music. This project bridges those two technologies together, allowing users to control and listen to their Spotify music through Spotify\s API while viewing synced lyrical and song information scraped from the web. This project was made for HackBeanPot 2018, using the Kivy framework and Python3.',
     image: './genius_icon.png'
   }
 ]
