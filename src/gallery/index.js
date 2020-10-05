7391b7bd374d69ddae64c88b038fcb288db70184
shane timmerman <shanetimmerman@gmail.com>
2020-10-05 00:09:19 -0400

    Added lightbox on click for gallery

--- src/gallery/index.js
+++ src/gallery/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 import PageContainer from '../components/page_container'
 import Gallery from 'react-photo-gallery';
+// import Carousel, { Modal, ModalGateway } from "react-images";
+import { Carousel } from 'react-responsive-carousel';
+import { Modal, Image } from 'semantic-ui-react'; 
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 
 const API_KEY = process.env.REACT_APP_IMGUR_CLIENT_ID
 
@@ -16,6 +20,8 @@ class Pics extends React.Component {
         height: 360,
         width: 540,
       }],
+      currentImage: 0,
+      viewerOpen: false,
     }
   }
 
@@ -36,11 +42,33 @@ class Pics extends React.Component {
     })
   }
 
+  openLightbox(event, { photo, index }) {
+    this.setState({ currentImage: index, viewerOpen: true });
+  }
+
+  closeLightbox() {
+    this.setState({ currentImage: 0, viewerOpen: false });
+  }
+
   render() {
     return (
-      <PageContainer textAlign='center'>
-        <Gallery photos={this.state.photos} direction={'column'} />  
-      </PageContainer>
+      <div>
+        <PageContainer textAlign='center'>
+          <Gallery photos={this.state.photos} direction={'column'} onClick={(event, { photo, index }) => (
+            this.setState({ currentImage: index, viewerOpen: true })
+          )} />  
+        </PageContainer>
+        <Modal className='carouselModal' open={ this.state.viewerOpen } 
+          onClose={() => this.setState({ viewerOpen: false, currentImage: 0})} >
+            <Carousel selectedItem={this.state.currentImage} 
+                showIndicators={false} infiniteLoop={false} dynamicHeight={true}
+                showThumbs={false} >
+              {this.state.photos.map(img => (
+                  <Image src={img.src} className='carouselImage' />
+              ))}
+            </Carousel>
+        </Modal>
+      </div>
     )
   }  
 }
